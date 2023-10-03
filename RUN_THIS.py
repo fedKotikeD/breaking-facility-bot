210f9359a6d5c1c0d00a1b324d5002b6efa73104
fedKotikeD <123323327+fedKotikeD@users.noreply.github.com>
2023-10-03 06:46:12 +0000

    Команда >status #3

--- RUN_THIS.py
+++ RUN_THIS.py
@@ -10,7 +10,7 @@ client = commands.Bot(command_prefix='>', intents=discord.Intents.all())
 
 @bot.event
 async def on_message(message):
-    if message.content.startswith("ping"): #crtl
+    if message.content.startswith("ping"):
         await message.channel.send("Pong!")
 
 @bot.event
@@ -18,12 +18,14 @@ async def on_ready():
     print("Бип-буп! Запуск успешный!")
     await bot.change_presence(activity=discord.Game(name="Breaking Facility"))
 
-@client.command
-async def hello(ctx):
-    await ctx.send("Hi!")
+# @client.command
+# async def hello(ctx):
+#     await ctx.send("Hi!")
 
-# @client.command()
-# async def ping(ctx):
-#     await ctx.send("Pong")
+server_status = requests.get('ip/status')
+@bot.event
+async def on_message(message):
+    if message.content.startswith("status"):
+        await message.channel.send(server_status)
 
 bot.run(bot_token)
